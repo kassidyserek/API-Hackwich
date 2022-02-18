96365bdafbb332a19508c1608bf50797c8ad357b
Kassidy <kqssidy@gmail.com>
2022-02-17 18:08:51 -0600

    Initial Commit

---
+++ API Hackwich/API_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_HackwichApp.swift
+//  API Hackwich
+//
+//  Created by KSerek on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct API_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
